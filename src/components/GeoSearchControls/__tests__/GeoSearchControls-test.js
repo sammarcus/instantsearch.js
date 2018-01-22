045ca35fdf11b406739b63095000e41ba61de372
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-22 17:38:03 +0100

    WIP

---
+++ src/components/GeoSearchControls/__tests__/GeoSearchControls-test.js
@@ -0,0 +1,29 @@
+// describe('GeoSearchRenderer - render', () => {
+//   describe('clear', () => {
+//     it('expect to render Button when enableClearMapRefinement is enabled');
+//
+//     it('expect to not render Button when enableClearMapRefinement is disabled');
+//   });
+//
+//   describe('control', () => {
+//     it(
+//       'expect to render Toggle when enableRefineControl is enabled with isRefineOnMapMove enabled'
+//     );
+//
+//     it(
+//       'expect to render Toggle when enableRefineControl is enabled and the map has not moved'
+//     );
+//
+//     it(
+//       'expect to render Button when enableRefineControl is enabled without isRefineOnMapMove enabled and the map has moved'
+//     );
+//
+//     it(
+//       'expect to render Button when enableRefineControl is not enabled with isRefineOnMapMove not enabled'
+//     );
+//
+//     it(
+//       'expect to render Button disabled when enableRefineControl is not enabled with isRefineOnMapMove enabled'
+//     );
+//   });
+// });
